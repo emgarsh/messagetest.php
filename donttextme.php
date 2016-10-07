90ff78b5f569012c94e0645042694b863a7bb691
Matthew Garcia <mgarcia@Matthews-MacBook-Pro-493.local>
2016-10-07 16:06:01 -0400

    dontextme

---
+++ donttextme.php
@@ -0,0 +1,11 @@
+<?php
+
+//prepare the BXML response headers
+header("content-type: text/xml");
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+//format response using transfer
+echo '<Response>';
+  echo '<SendMessage to="'.$_REQUEST['from'].'" from="'.$_REQUEST['to'].'">DontTextMe</SendMessage>';
+echo '</Response>';
+?>
